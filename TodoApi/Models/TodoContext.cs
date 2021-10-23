dd9f1d0e7dd8b54f76246f5ed77cea488745cd82
Syed Malik <ayazsah@gmail.com>
2021-10-23 15:58:48 -0400

    Update TodoContext Class

--- TodoApi/Models/TodoContext.cs
+++ TodoApi/Models/TodoContext.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace TodoApi.Models
 {
-    public class TodoContext
+    public class TodoContext : DbContext
     {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
     }
 }
